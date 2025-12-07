97d266f90b35b6e3a4e2fb900e73048ec27d525f
Uladzislau Blok <blokv75@gmail.com>
2025-12-07 17:51:58 +0100

    client server forwarding

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,95 @@
 mod socket_channel;
 
+use log::{error, info, trace};
+use std::thread;
 use std::{
-    io::Read,
-    net::{SocketAddr, TcpListener, TcpStream},
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
 };
 
-fn handle_client((mut stream, addr): (TcpStream, SocketAddr)) -> std::io::Result<()> {
-    println!("Read bytes from {addr}");
-    let mut count = 0;
+fn handle_client(
+    mut client: &TcpStream,
+    client_addr: &str,
+    mut server: &TcpStream,
+    server_addr: &str,
+) {
     loop {
-        let mut tmp_buf = [0u8; 1024];
-        match stream.read(&mut tmp_buf) {
+        let mut buf_req = [0u8; 8192];
+        match client.read(&mut buf_req) {
             Ok(0) => {
-                println!("Read 0 bytes. Will stop");
+                info!(
+                    "Client {client_addr} has closed connection. Close server connection {server_addr}"
+                );
                 break;
             }
             Ok(bytes) => {
-                count += 1;
-                println!("Read {bytes} bytes; {count}");
-
-                println!(
+                info!("Forward {bytes} bytes from {client_addr} to {server_addr}");
+                trace!(
                     "{}",
-                    &tmp_buf[..bytes]
+                    &buf_req[..bytes]
                         .iter()
                         .map(|b| format!("{b:02x}").to_string())
                         .collect::<Vec<String>>()
                         .join(" ")
                 );
+                match server.write(&buf_req[..bytes]) {
+                    Ok(__) => {}
+                    Err(__) => {}
+                }
             }
             Err(e) => {
-                return Err(e);
+                error!("Unexpected error while reading from {client_addr} : {e}");
+            }
+        };
+
+        let mut buf_resp = [0u8; 8192];
+        match server.read(&mut buf_resp) {
+            Ok(0) => {
+                info!(
+                    "Server {server_addr} has closed connection. Close client connection {client_addr}"
+                );
+                break;
+            }
+            Ok(bytes) => {
+                info!("Forward {bytes} bytes from {server_addr} to {client_addr}");
+                trace!(
+                    "{}",
+                    &buf_req[..bytes]
+                        .iter()
+                        .map(|b| format!("{b:02x}").to_string())
+                        .collect::<Vec<String>>()
+                        .join(" ")
+                );
+                match client.write(&buf_resp[..bytes]) {
+                    Ok(__) => {}
+                    Err(__) => {}
+                }
+            }
+            Err(e) => {
+                error!("Unexpected error while reading from {server_addr} : {e}");
             }
         }
     }
-    println!("Close connection");
-    stream.shutdown(std::net::Shutdown::Both)?;
-    Ok(())
+}
+
+fn init_logger() {
+    env_logger::Builder::from_default_env()
+        .filter_level(log::LevelFilter::Trace)
+        .init();
 }
 
 fn main() -> std::io::Result<()> {
+    init_logger();
+    info!("Start proxy");
+    let server_addr = "127.0.0.1:8086";
     let listener = TcpListener::bind("127.0.0.1:8085")?;
-    handle_client(listener.accept()?)?;
+    let (client, client_addr) = listener.accept()?;
+    let client_addr_str = client_addr.to_string();
+    let server = TcpStream::connect(server_addr)?;
+    info!("Start client - server connection");
+    thread::spawn(move || {
+        handle_client(&client, &client_addr_str, &server, server_addr);
+    })
+    .join();
     Ok(())
 }
